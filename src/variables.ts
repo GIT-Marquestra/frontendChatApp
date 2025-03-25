97fc8087fbe9843d7fd852d501e176638a8a41d5
GIT-Marquestra <vabhi.verma2678@gmail.com>
2025-03-26 03:34:00 +0530

    chnaged cors options

--- src/variables.ts
+++ src/variables.ts
@@ -1,4 +1,4 @@
 export const cloudinaryString="cloudinary://<your_api_key>:<your_api_secret>@dutrfbtao"
-export const backendString="backend-chat-app-production-05a8.up.railway.app"
+export const backendString="https://backend-chat-app-production-05a8.up.railway.app"
 export const wsString="https://chatappwebsocket-8.onrender.com"
 // nice
