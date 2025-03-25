77c88b23d68e1770fe17c0617ca78de0d8981f80
GIT-Marquestra <vabhi.verma2678@gmail.com>
2025-03-25 22:43:29 +0530

    done

--- src/variables.ts
+++ src/variables.ts
@@ -1,3 +1,3 @@
 export const cloudinaryString="cloudinary://<your_api_key>:<your_api_secret>@dutrfbtao"
-export const backendString="https://backend-chat-app-production.up.railway.app/"
+export const backendString="https://backend-chat-app-production.up.railway.app"
 export const wsString="https://chatappwebsocket-8.onrender.com"
